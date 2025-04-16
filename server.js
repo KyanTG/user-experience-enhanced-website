244f9c453874cc69393f1d29bd4b3aec8cb4414a
KyanTG <kyan.gijtenbeek@hva.nl>
2025-04-16 20:24:26 +0200

    express en public toegevoegd

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+// importeert de benodigde apps
 
 import express from 'express';
 import { Liquid } from 'liquidjs';
+
+// zorgt dat de public map gebruikt word voor content
+
+const app = express();
+app.use(express.static('public'))
+
 
 
 

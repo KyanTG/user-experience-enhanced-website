5a72f5610c228952d81382707ca58a934c84017c
KyanTG <kyan.gijtenbeek@hva.nl>
2025-04-24 15:09:07 +0200

    now the states work correctly

--- server.js
+++ server.js
@@ -30,13 +30,18 @@ const RadioJSON = await Radio.json()
 // veronica page of homepage
 app.get('/veronica', async function (request, response) {
 
+  // nieuwe fetch met width en height van cover https://fdnd-agency.directus.app/items/mh_shows?fields=id,from,until,show.id,show.name,show.body,show.radiostation.name,show.users.mh_users_id.full_name,show.users.mh_users_id.cover.id,show.users.mh_users_id.cover.width,show.users.mh_users_id.cover.height&filter={%22show%22:{%22radiostation%22:{%22name%22:%22Radio%20Veronica%22}}}
+  
   const algemeenVeronica = await fetch('https://fdnd-agency.directus.app/items/mh_shows?fields=id,from,until,show.id,show.name,show.body,show.radiostation.name,show.users.mh_users_id.full_name,show.users.mh_users_id.cover&filter={%22show%22:{%22radiostation%22:{%22name%22:%22Radio%20Veronica%22}}}&')
   const algemeenVeronicaJSON = await algemeenVeronica.json()
+
+  const likedShows = await fetch('https://fdnd-agency.directus.app/items/mh_accounts/7?fields=id,name,liked_shows.mh_show_id.*.*.*')
+  const likedShowsJSON = await likedShows.json()
 
   const radioStations = await fetch('https://fdnd-agency.directus.app/items/mh_radiostations')
   const radioStationsJSON = await radioStations.json()
 
-  response.render('veronica.liquid', {algemeen: algemeenVeronicaJSON.data, radio:radioStationsJSON} )
+  response.render('veronica.liquid', {algemeen: algemeenVeronicaJSON.data, radio:radioStationsJSON, likes:likedShowsJSON.data} )
 })
 
 // days data komt hier
@@ -51,7 +56,7 @@ response.render('veronica-likes.liquid', {algemeen: likedShowsJSON.data} )  // h
 })
 
 // data & likes post
-app.post('/veronica', async function (request, response) {
+app.post('/veronica/like', async function (request, response) {
 
   //console.log(request.body) dit is om te checken of het werkt
   const testConsole = await fetch('https://fdnd-agency.directus.app/items/mh_accounts_shows', {
@@ -68,6 +73,18 @@ app.post('/veronica', async function (request, response) {
     response.redirect(303, '/veronica' )  // hierdoor word je teruggestuurd naar de homepage nadat je geliked hebt
 })
 
+
+// app.post('/veronica/likes', async function (request, response) {
+
+//   const deleteLike = await fetch('https://fdnd-agency.directus.app/items/mh_accounts/7?fields=id,name,liked_shows.mh_show_id.id', {
+//     method: 'DELETE',         
+//     headers: {
+//       'Content-Type': 'application/json;charset=UTF-8'
+//     },
+//   })
+
+//   response.redirect(303, '/veronica/likes' )  // hierdoor word je teruggestuurd naar de likes page nadat je een like hebt verwijderd
+// })
 
 // error page
 app.use((req, res, next) => {
